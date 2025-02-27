b35a54050cf51715e45789063ac6b1bdc8fc33a7
malei <435012357@qq.com>
2025-02-27 15:37:15 +0800

    docs: docs的国际化

--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -9,12 +9,37 @@ export default defineConfig({
     light: '/rspress-light-logo.png',
     dark: '/rspress-dark-logo.png',
   },
+  lang: 'zh',
+  locales: [
+    {
+      lang: 'en',
+      label: 'English',
+      title: 'Bubbles',
+      description: 'Bubbles is a modern, lightweight, and fast component library.',
+    },
+    {
+      lang: 'zh',
+      label: '简体中文',
+      title: 'Bubbles',
+      description: 'Bubbles 是一个现代、轻量级且快速的组件库。',
+    },
+  ],
   themeConfig: {
     socialLinks: [
       {
         icon: 'github',
         mode: 'link',
         content: 'https://github.com/web-infra-dev/rspress',
+      },
+    ],
+    locales: [
+      {
+        lang: 'en',
+        label: 'On this page',
+      },
+      {
+        lang: 'zh',
+        label: '大纲',
       },
     ],
   },
