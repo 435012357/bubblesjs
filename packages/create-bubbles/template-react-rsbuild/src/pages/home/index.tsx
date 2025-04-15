fb291512d7d4653781983e81cd655940c56ea98b
malei <435012357@qq.com>
2025-04-15 13:37:48 +0800

    chore(create-bubbles): 更新模板项目配置

--- packages/create-bubbles/template-react-rsbuild/src/pages/home/index.tsx
+++ packages/create-bubbles/template-react-rsbuild/src/pages/home/index.tsx
@@ -1,26 +1,5 @@
-import { use } from 'react';
-
-const fetchData = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        data: [
-          {
-            id: 1,
-            title: 'Album 1',
-          },
-          {
-            id: 2,
-            title: 'Album 2',
-          },
-        ],
-      });
-    }, 1000);
-  });
-};
-
 const Home = () => {
-  return <h1>111</h1>;
-};
+  return <h1 className="text-red text-30px">111</h1>
+}
 
-export default Home;
+export default Home
